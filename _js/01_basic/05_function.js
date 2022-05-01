c635fb0c84d04b7c83aa0ac761dad8722ec08d2d
woojin999 <24864@naver.com>
2022-05-01 17:39:13 +0900

    5

---
+++ _js/01_basic/05_function.js
@@ -0,0 +1,7 @@
+console.log("-------------------------");
+console.log("fuction.js에서 실행되는 코드");
+console.log("-------------------------");
+
+function outerFunc() {
+    console.log("functiom.js의 outerFunc에서 실행되는 코드");
+}
