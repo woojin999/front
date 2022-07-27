c90360c2d612e7db8b745538cc18ea8c3cdd79dd
woojin999 <24864@naver.com>
2022-07-27 14:12:23 +0900

    3

---
+++ _js/practic/19_json_interation/index.js
@@ -0,0 +1,32 @@
+const exs = {
+    exchanges: [
+        {name: '미국USD',
+        point: 1227.00,
+        updown: -5.00
+        }, 
+        {name: '일본JPY',
+        point: 973.23,
+        updown: -7.94
+        }, 
+        {name: '유럽연합EUR',
+        point: 1329.70,
+        updown: -8.93
+        }, 
+        {name: '중국CNY',
+        point: 192.48,
+        updown: -0.62
+        }
+    ],
+   gold: [
+        {
+            name: '국제금(04.12)',
+            point: 1972.10,
+            updown: 27.80
+        },
+        {
+            name: '국내금(04.13)',
+            point: 77765.54,
+            updown: 495.20
+        }
+    ]
+};
