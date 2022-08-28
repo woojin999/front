412f96e2842939f6d4a44aaddc9b09471fc2100f
woojin999 <24864@naver.com>
2022-08-28 23:51:23 +0900

    front
    front

---
+++ _js/01_basic/05_function2.js
@@ -0,0 +1,21 @@
+function myFunc1() {
+    console.log('너무 선언식이 많으면 따로 js파일 만들어서 선언식을 모아둔다!')
+}
+function myFunc2() {
+    console.log('너무 선언식이 많으면 따로 js파일 만들어서 선언식을 모아둔다!')
+}
+function myFunc3() {
+    console.log('너무 선언식이 많으면 따로 js파일 만들어서 선언식을 모아둔다!')
+}
+function myFunc4() {
+    console.log('너무 선언식이 많으면 따로 js파일 만들어서 선언식을 모아둔다!')
+}
+function myFunc5() {
+    console.log('너무 선언식이 많으면 따로 js파일 만들어서 선언식을 모아둔다!')
+}
+function myFunc6() {
+    console.log('너무 선언식이 많으면 따로 js파일 만들어서 선언식을 모아둔다!')
+}
+function myFunc7() {
+    console.log('너무 선언식이 많으면 따로 js파일 만들어서 선언식을 모아둔다!')
+}
